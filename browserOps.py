0ac8d2ae3953fc40b01a41f00156bcb2763d3d1e
Donnell <donnedebnam1@aol.com>
2018-12-15 02:56:23 -0500

    updt

--- browserOps.py
+++ browserOps.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 
 def add_to_bag(url):
-
+    # Boot up webdriver; process adidas url
     driver = webdriver.Firefox()
     driver.get(url)
+    # Grab CSS to "Add to Bag" button
     btn = driver.find_element_by_css_selector('.gl-cta.gl-cta--primary.gl-cta--full-width.btn-bag')
+    # Click button
     btn.click()
-    
